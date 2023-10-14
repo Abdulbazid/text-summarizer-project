b34a06e649fe6ad9c6cf2d9032ab8dfee8efc899
Abdul bazid <abdulbazid4@gmail.com>
2023-10-14 20:16:37 +0530

    data ingestion added

---
+++ src/textSummarizer/constants/__init__.py
@@ -0,0 +1,4 @@
+from pathlib import Path
+
+CONFIG_FILE_PATH = Path(r"C:\Users\Abdul\Desktop\text\text-summarizer-project\config\config.yaml")
+PARAMS_FILE_PATH = Path(r"C:\Users\Abdul\Desktop\text\text-summarizer-project\param.yaml")
